637cfe58f16ea1c260781b72e491050ad3d1dc9f
Maria Branca <m.branca@ikom.it>
2016-02-03 12:01:35 +0100

    ..

--- src/Ikom/CmsList.php
+++ src/Ikom/CmsList.php
@@ -47,7 +47,7 @@ class CmsList extends AbstractMagentoCommand{
             }
             #$page = $page->getAllIds();
             $intro = 'Lista delle pagine statiche';
-            file_put_contents('$filename', $intro);
+            file_put_contents($filename, $intro);
            	foreach($page as $collection){
 	            	$page->load();
 	            	$data = json_encode($page->getData(), JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
